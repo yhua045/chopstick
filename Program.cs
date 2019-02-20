b03ec40a47524df857746feac4315b625c0fa293
Kyle Huang <hkyl_xtra@hotmail.com>
2019-02-20 18:02:10 +1100

    Enable application insights

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Chopstick
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseApplicationInsights();
     }
 }
